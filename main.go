324daeec81a2d5ab1b272f8010aac20cb2c14105
gordes317 <33797010+gordes317@users.noreply.github.com>
2021-07-21 20:57:53 +0800

    Update main.go

--- main.go
+++ main.go
@@ -5,7 +5,11 @@ import "fmt"
 func sum(a, b, c int) int {
 	return a + b + c
 }
+func sub(a, b, c int) int {
+	return a - b - c
+}
 
 func main() {
 	fmt.Println("Sum: ", sum(1, 2, 3))
+	fmt.Println("Sub: ", sub(10, 2, 3))
 }
